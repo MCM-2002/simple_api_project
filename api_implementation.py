32bf4986951a568a7ceeddadfe3305626adb3a1e
chamith madusanka <chamithfree3@gmail.com>
2025-09-13 10:03:52 +0530

    Update api_implementation.py

--- api_implementation.py
+++ api_implementation.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 11 19:14:44 2022
 
-@author: siddhardhan
-"""
 
 
 import json
@@ -28,5 +23,6 @@ input_data_for_model = {
 input_json = json.dumps(input_data_for_model)
 
 response = requests.post(url, data=input_json)
+
 
 
